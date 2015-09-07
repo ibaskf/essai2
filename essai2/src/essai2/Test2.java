4b5955218accf86ab4f677439b3ab40342c651d2
stagiaire <stagiaire@Stagiaire03>
2015-09-07 11:50:26 +0200

    tt

--- essai2/src/essai2/Test2.java
+++ essai2/src/essai2/Test2.java
@@ -1,5 +1,15 @@
 package essai2;
 
 public class Test2 {
+	private String test2;
 
+	public String getTest2() {
+		return test2;
+	}
+
+	public void setTest2(String test2) {
+		this.test2 = test2;
+	}
+
+	
 }
