fcf264f8af0a85abcaf4d02756c565a2ec039b4d
stagiaire <stagiaire@Stagiaire03>
2015-09-07 12:02:14 +0200

    1

--- essai2/src/essai2/Test.java
+++ essai2/src/essai2/Test.java
@@ -1,19 +1,19 @@
 package essai2;
 
 public class Test {
-	private String test;
+	private String test1;
 
 	public String getTest() {
-		return test;
+		return test1;
 	}
 
 	public void setTest(String test) {
-		this.test = test;
+		this.test1 = test;
 	}
 
 	@Override
 	public String toString() {
-		return "Test [test=" + test + "]";
+		return "Test [test=" + test1 + "]";
 	}
 	
 
